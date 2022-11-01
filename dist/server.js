c391345d786c342068b447801c8f8ba06b51b5f9
yepMad <eudestfilho@gmail.com>
2022-11-01 20:18:38 -0300

    (fix): added .gitignore

--- dist/server.js
+++
@@ -1,6 +0,0 @@
-"use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
-
-const port = process.env.PORT || 3000;
-_app2.default.listen(port, () => {
-  console.log('Umbler listening on port %s', port);
-});
