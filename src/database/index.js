63d25dfd62c91a6322ef079f6b6a397500280449
yepMad <eudestfilho@gmail.com>
2022-11-01 20:18:53 -0300

    (feature): added .env.example

--- src/database/index.js
+++ src/database/index.js
@@ -6,15 +6,12 @@ class Database {
   }
 
   mongo() {
-    this.mongoConnection = mongoose.connect(
-      'mongodb://localhost:27017/nonsenseasteroid',
-      {
-        useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-      }
-    );
+    this.mongoConnection = mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
   }
 }
 
